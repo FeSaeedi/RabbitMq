4dcb1835a12a3dc08955d28e19c163ff751cc0e9
Fereshte Saeedi <f.saeedi85@yahoo.com>
2019-07-21 11:21:42 +0430

    Init

---
+++ WorkerQueues/NewTask/NewTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = connectionFactory.CreateConnection())
+            using (var chanel = connection.CreateModel())
+            {
+                chanel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                string message = GetMesage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+                var properties = chanel.CreateBasicProperties();
+                properties.Persistent = true;
+                chanel.BasicPublish(exchange: "", routingKey: "task_queu", basicProperties: properties, body: body);
+            }
+        }
+        private static string GetMesage(string[] args)
+        {
+            return (args.Length > 0) ? string.Join(" ", args) : "Hello World";
+        }
+    }
+}
---
+++ WorkerQueues/Worker1/Worker1/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Worker1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = connectionFactory.CreateConnection())
+            using (var chanel = connection.CreateModel())
+            {
+                chanel.QueueDeclare(queue: "task_queu", durable: false, exclusive: false, autoDelete: false, arguments: null);
+              
+                var consumer = new EventingBasicConsumer(chanel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[x] Recived {0}", message);
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+                    Console.WriteLine("[x] Done");
+                };
+                chanel.BasicConsume(queue: "task_queu", autoAck: true, consumer: consumer);
+                Console.WriteLine("Press enu Key");
+                Console.ReadLine();
+            }
+        }
+    }
+}
